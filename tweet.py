4ad46e7d4f346aac162b1f1be76761b37ff93c2b
UfukYarisan <yarisan17@itu.edu.tr>
2022-07-19 23:41:58 +0300

    FIX(scraper): changed query and csv structure
    Stocks are listed. For each seperate stock different query and csv file is created.
    
    Coded-by: Ufuk Yarışan

--- tweet.py
+++ tweet.py
@@ -2,50 +2,42 @@ import snscrape.modules.twitter as twitter
 import pandas as pd
 from tqdm import tqdm
 
+stocks = ["BIST30", "XU030", "AKBNK", "ARCLK", "ASELS", "BIMAS",
+                          "EKGYO", "EREGL", "FROTO", "GUBRF", "SAHOL", "HEKTS",
+                          "KRDMD", "KCHOL", "KOZAL", "KOZAA", "PGSUS", "SASA",
+                          "PETKM", "TAVHL", "TKFEN", "TOASO", "TCELL", "TUPRS",
+                          "THYAO","TTKOM", "GARAN", "HALKB", "ISCTR", "SISE",
+                          "VESTL", "YKBNK"]
+for stock in stocks:
+    query = (f"{stock}(#{stock} OR #${stock}) lang:tr")
+    tweets = []
+    limit = 1000
+    for tweet in tqdm(twitter.TwitterSearchScraper(query).get_items()):
+        if(len(tweets) < limit):
+            tweets.append(
+                [
+                    tweet.date,
+                    tweet.user.username,
+                    tweet.content,
+                    tweet.retweetCount,
+                    tweet.likeCount,
+                    tweet.user.followersCount,
+                    tweet.user.verified,
 
-"""
-query = (
-    "(#BIST30 OR #XU030 OR #AKBNK OR #ARCLK OR #ASELS OR #BIMAS OR"
-    " #EKGYO OR #EREGL OR #FROTO OR #GUBRF OR #SAHOL OR #HEKTS OR #KRDMD OR"
-    " #KCHOL OR #KOZAL OR #KOZAA OR #PGSUS OR #SASA OR #PETKM OR #TAVHL OR"
-    " #TKFEN OR #TOASO OR #TCELL OR #TUPRS OR #THYAO OR #TTKOM OR #GARAN OR"
-    " #HALKB OR #ISCTR OR #SISE OR #VESTL OR #YKBNK) lang:tr until:2022-07-12"
-    " since:2021-01-01 -filter:links -filter:replies"
-)
+                    tweet.hashtags,
+                    tweet.cashtags,
+                    tweet.retweetedTweet,
+                    tweet.quotedTweet,
+                    tweet.media
+                ]
+            )
+        else:
+            break
 
-"""
+    df = pd.DataFrame(
+        tweets, columns=["date", "user", "tweet", "rt", "fav", "followers", "verified",
+                         "hashtags", "cashtags", "retweetedTweet", "quotedTweet", "media"]
+    )
 
-query = (
-    "(#BIST30 OR #XU030 OR #AKBNK OR #ARCLK OR #ASELS OR #BIMAS OR"
-    " #EKGYO OR #EREGL OR #FROTO OR #GUBRF OR #SAHOL OR #HEKTS OR #KRDMD OR"
-    " #KCHOL OR #KOZAL OR #KOZAA OR #PGSUS OR #SASA OR #PETKM OR #TAVHL OR"
-    " #TKFEN OR #TOASO OR #TCELL OR #TUPRS OR #THYAO OR #TTKOM OR #GARAN OR"
-    " #HALKB OR #ISCTR OR #SISE OR #VESTL OR #YKBNK) until:2021-01-01"
-    " since:2019-01-01 -filter:replies"
-)
-tweets = []
-limit = 1000000
-
-for tweet in tqdm(twitter.TwitterSearchScraper(query).get_items()):
-    if len(tweets) < limit:
-        tweets.append(
-            [
-                tweet.date,
-                tweet.user.username,
-                tweet.content,
-                tweet.retweetCount,
-                tweet.likeCount,
-                tweet.user.followersCount,
-                tweet.user.verified,
-            ]
-        )
-    else:
-        break
-
-df = pd.DataFrame(
-    tweets, columns=["date", "user", "tweet", "rt", "fav", "followers", "verified"]
-)
-
-# csv
-# df.to_csv('s.csv', index=False)
-# print(df.head(5))
+    # csv
+    df.to_csv(f'./data/stock_tweets/{stock}.csv', index=False)
